3616c5f0650b53f1fa79f4c4e393c68416959a4c
Eddie Hart <157631895+juneberryredbull@users.noreply.github.com>
2024-01-31 11:53:30 -0700

    made return methods instead of void methods

--- src/Circleguy.java
+++ src/Circleguy.java
@@ -5,29 +5,30 @@
 import java.util.Scanner;
 
 public class Circleguy {
-    public static void calculateDiameter(double d) {
-        double diameter = d * 2;
-        System.out.println("Diameter: " + diameter);
+    public static double calculateDiameter(double d) {
+        return d * 2;
     }
-// This method calculates the diameter
-    public static void  calculateArea(double a) {
-        double area = (a * a) * Math.PI;
-        System.out.printf("Area: %.2f\n", area);
+    // This method calculates the diameter
+    public static double calculateArea(double a) {
+        return (a * a) * Math.PI;
     }
-// This method calculates the area
-    public static void calculateCircumference(double c) {
-        double circumference = Math.PI * 2 * c;
-        System.out.printf("Circumference: %.2f", circumference);
+    // This method calculates the area
+    public static double calculateCircumference(double c) {
+        return Math.PI * 2 * c;
     }
-// This method calculates the circumference
+    // This method calculates the circumference
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter circle's radius: ");
         double radius = input.nextDouble();
-        calculateDiameter(radius);
-        calculateArea(radius);
-        calculateCircumference(radius);
+        double diameter = calculateDiameter(radius);
+        double area = calculateArea(radius);
+        double circumference = calculateCircumference(radius);
+        System.out.printf("Diameter: %.2f \n", diameter);
+        System.out.printf("Area: %.2f \n", area);
+        System.out.printf("Circumference: %.2f", circumference);
         // here in main i use all the methods i defined to calculate different values
         // from the user's radius input
+        // i also made sure these are now return methods instead of void methods
     }
 }
